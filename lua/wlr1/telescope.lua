7f529189c231abb84e3c3c1b88c9b120513f48ae
wlr1 <uns4d123@gmail.com>
2025-04-25 13:03:16 +0300

    yazi

--- lua/wlr1/telescope.lua
+++ lua/wlr1/telescope.lua
@@ -1,6 +1,48 @@
-require("telescope").setup({
+-- lua/wlr1/telescope.lua
+local telescope = require("telescope")
+local actions = require("telescope.actions")
+local builtin = require("telescope.builtin")
+
+telescope.setup({
   defaults = {
     hidden = true,
     no_ignore = false,
+    mappings = {
+      i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-c>"] = actions.close,
+        ["<CR>"] = actions.select_default,
+      },
+      n = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["q"] = actions.close,
+      },
+    },
+  },
+  extensions = {
+    -- ja izmanto file_browser vai zoxide, piereģistrē šeit
+    file_browser = {},
+    zoxide = {},
   },
 })
+
+-- ja izmanto Telescope extensions, piemēram:
+--   require("telescope").load_extension("file_browser")
+--   require("telescope").load_extension("zoxide")
+
+-- globālās taustiņu saīsnes:
+vim.keymap.set("n", "<leader>jk", function()
+  builtin.find_files({
+    find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+  })
+end, { desc = "🔍 Find Files (rg, hidden)" })
+
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope file_browser<CR>", { desc = "📂 File Browser" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "🔍 Live Grep" })
+vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "🔍 Diagnostics" })
+vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, { desc = "🔍 LSP Document Symbols" })
+vim.keymap.set("n", "<leader>ws", builtin.lsp_workspace_symbols, { desc = "🔍 LSP Workspace Symbols" })
+vim.keymap.set("n", "<leader>fz", "<cmd>Telescope zoxide list<CR>", { desc = "🔍 Zoxide List" })
+vim.keymap.set("n", "<leader>fv", builtin.help_tags, { desc = "🔍 Help Tags" })
